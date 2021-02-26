e9725a98e780ca035bb321cb525adedb9489c09e
Gary Raven <67789434+jokyjoe-joy@users.noreply.github.com>
2021-02-26 11:01:53 +0100

    Added enemy pointer

---
+++ Assets/SpaceShipPack/Scripts/EnemyPointer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPointer : MonoBehaviour
+{
+    [SerializeField] private Camera uiCamera;
+    private Vector3 targetPosition;
+    private RectTransform pointerRectTransform;
+
+    private void Awake() {
+
+        pointerRectTransform = transform.Find("Pointer").GetComponent<RectTransform>();
+    }
+
+    private void Update() {
+        GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");
+        if (enemy == null) {
+            pointerRectTransform.localScale = new Vector3(0,0,0);
+            return;
+        }
+        targetPosition = enemy.transform.position;
+
+        Vector3 toPosition = targetPosition;
+        Vector3 fromPosition = Camera.main.transform.position;
+        Debug.DrawLine(fromPosition, toPosition, Color.green); 
+        //fromPosition.z = 0f; // ???
+
+        Vector3 targetPositionViewPoint = Camera.main.WorldToViewportPoint(targetPosition);
+        bool isOffScreen = targetPositionViewPoint.x >= 0 && targetPositionViewPoint.x <= 1 && targetPositionViewPoint.y >= 0 && targetPositionViewPoint.y <= 1 && targetPositionViewPoint.z <= 0;
+
+        if (isOffScreen) {
+            pointerRectTransform.localScale = new Vector3(0,0,0);
+        } else {
+            pointerRectTransform.localScale = new Vector3(0.3f,0.3f,0.3f);
+            Vector3 myPos = Camera.main.WorldToScreenPoint(targetPosition);
+            pointerRectTransform.position = myPos;
+        }
+    }
+}
