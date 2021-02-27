905321db33c5d9f8336cf1d67a7c9ae84090db58
Gary Raven <67789434+jokyjoe-joy@users.noreply.github.com>
2021-02-27 15:44:08 +0100

    Added healthBar percent text

---
+++ Assets/SpaceShipPack/Scripts/UI/HealthBarPercent.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarPercent : MonoBehaviour
+{
+    private HealthData playerHealthData;
+    private Text healthPercentText;
+    private void Awake() {
+        GameObject player = GameObject.FindGameObjectWithTag("PlayerTag").transform.parent.gameObject;
+        playerHealthData = player.GetComponent<HealthData>();
+        healthPercentText = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        healthPercentText.text = (playerHealthData.health / playerHealthData.healthMax * 100).ToString() + "%";
+    }
+}
