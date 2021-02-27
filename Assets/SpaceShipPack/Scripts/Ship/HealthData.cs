5bd444ad469478690aaedfef3f759f8f85b6d146
Gary Raven <67789434+jokyjoe-joy@users.noreply.github.com>
2021-02-27 10:18:06 +0100

    Fixed health going negative

--- Assets/SpaceShipPack/Scripts/Ship/HealthData.cs
+++ Assets/SpaceShipPack/Scripts/Ship/HealthData.cs
@@ -5,13 +5,13 @@ using UnityEngine.Events;
 
 public class HealthData : MonoBehaviour
 {
-
     public float health = 100f;
     public float healthMax = 100f;
     [HideInInspector] public UnityEvent onDamage;
     
     public void Damage(float damageAmount) {
         health -= damageAmount;
+        if (health < 0) health = 0;
         onDamage.Invoke();
     }
 
