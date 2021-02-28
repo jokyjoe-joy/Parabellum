99df6f4b7313be5ef89d642d260e2f918cff8aa9
Gary Raven <67789434+jokyjoe-joy@users.noreply.github.com>
2021-02-28 12:11:58 +0100

    Collectable moving towards object with inventory

--- Assets/SpaceShipPack/Scripts/Inventory/CollectableToInventory.cs
+++ Assets/SpaceShipPack/Scripts/Inventory/CollectableToInventory.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 public class CollectableToInventory : MonoBehaviour
 {
     public ItemObject item;
+    private Transform closeObjectTransform = null;
+    public float fromDistanceToAddToInventory = 10f;
+    public float maxSpeed = 0.8f;
+    private InventoryObject inventory = null;
+    
+    private void Update() {
+        if (closeObjectTransform != null) {
+            // If collector is close enough then add the item to its inventory
+            // Otherwise move closer to the object
+            float distanceFromObject = Vector3.Distance(transform.position, closeObjectTransform.position);
+            if (distanceFromObject < fromDistanceToAddToInventory) {
+                inventory.AddItem(item, 1);
+                Destroy(gameObject);
+            } else {
+                float step = Mathf.Clamp(30 * Time.deltaTime - distanceFromObject / 50, 0, maxSpeed);
+                if (step > 0) transform.position = Vector3.MoveTowards(transform.position, closeObjectTransform.position, step);
+            }
+        }
+    }
 
     public void OnTriggerEnter(Collider other) {
-        InventoryObject inventory = null;
+        // Check if this object has an inventory, if it doesn't: return.
         // TODO: also try to get the inventory of AI
-        ShipController shipController = other.GetComponent<ShipController>();
+        ShipController shipController = other.transform.GetComponent<ShipController>();
         if (shipController != null) {
             inventory = shipController.inventory;
         } else {
@@ -22,7 +41,12 @@ public class CollectableToInventory : MonoBehaviour
         }
         if (inventory == null) return;
 
-        inventory.AddItem(item, 1);
-        Destroy(gameObject);
+        closeObjectTransform = other.transform;
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.transform == closeObjectTransform) {
+            closeObjectTransform = null;
+        }
     }
 }
