0707bf3771dec98f6cb13420d98996c2593dffdc
Gary Raven <67789434+jokyjoe-joy@users.noreply.github.com>
2021-03-07 18:42:10 +0100

    Added item tooltips to inventory UI.

--- Assets/SpaceShipPack/Scripts/Inventory/UserInterface.cs
+++ Assets/SpaceShipPack/Scripts/Inventory/UserInterface.cs
@@ -12,6 +12,8 @@ public abstract class UserInterface : MonoBehaviour
     protected Transform itemSlotTemplate;
     private bool isInventoryActive;
     private ShipController playerShipController;
+    public GameObject itemTooltipPrefab;
+    GameObject currentItemTooltip;
 
     private void Awake()
     {
@@ -124,10 +126,37 @@ public abstract class UserInterface : MonoBehaviour
     public void OnEnter(GameObject obj)
     {
         MouseData.slotHoveredOver = obj;
+
+        // If there is an item in the slot, display a UI tooltip for it.
+        if (slotsOnInterface[obj].ItemObject != null && currentItemTooltip == null)
+        {
+            // Put the tooltip a little bit above cursor.
+            Vector3 positionToInstantiate = obj.transform.position;
+            positionToInstantiate.x += 100;
+
+            // Instantiate tooltip and set text and description accordingly.
+            currentItemTooltip = Instantiate(itemTooltipPrefab, positionToInstantiate, Quaternion.identity);
+            currentItemTooltip.transform.SetParent(transform.parent);
+
+            if (!currentItemTooltip.GetComponent<RectTransform>().IsFullyVisibleFrom(Camera.main))
+            {
+                // TODO: Fix this and place tooltip so that it is visible.
+                Debug.Log("not visible");
+            }
+
+            Text name = currentItemTooltip.transform.Find("name").GetComponent<Text>();
+            Text description = currentItemTooltip.transform.Find("description").GetComponent<Text>();
+
+            name.text = slotsOnInterface[obj].ItemObject.itemName;
+            description.text = slotsOnInterface[obj].ItemObject.itemDescription;
+        }
     }
     public void OnExit(GameObject obj)
     {
+        // FIXME: Can't select two items after each other.
         MouseData.slotHoveredOver = null;
+        // If there is a tooltip instantiated, destroy it.
+        if (currentItemTooltip != null) Destroy(currentItemTooltip);
     }
 
     public void OnEnterInterface(GameObject obj)
@@ -181,6 +210,8 @@ public abstract class UserInterface : MonoBehaviour
         if (MouseData.tempItemBeingDragged != null)
             MouseData.tempItemBeingDragged.GetComponent<RectTransform>().position = Input.mousePosition;
     }
+
+
 }
 
 public static class MouseData
@@ -197,5 +228,57 @@ public static class ExtensionMethods
         foreach (KeyValuePair<GameObject, InventorySlot> _slot in _slotsOnInterface)
         {
         }
+    }
+}
+ 
+public static class RendererExtensions
+{
+    /// <summary>
+    /// Counts the bounding box corners of the given RectTransform that are visible from the given Camera in screen space.
+    /// </summary>
+    /// <returns>The amount of bounding box corners that are visible from the Camera.</returns>
+    /// <param name="rectTransform">Rect transform.</param>
+    /// <param name="camera">Camera.</param>
+    private static int CountCornersVisibleFrom(this RectTransform rectTransform, Camera camera)
+    {
+        Rect screenBounds = new Rect(0f, 0f, Screen.width, Screen.height); // Screen space bounds (assumes camera renders across the entire screen)
+        Vector3[] objectCorners = new Vector3[4];
+        rectTransform.GetWorldCorners(objectCorners);
+ 
+        int visibleCorners = 0;
+        Vector3 tempScreenSpaceCorner; // Cached
+        for (var i = 0; i < objectCorners.Length; i++) // For each corner in rectTransform
+        {
+            tempScreenSpaceCorner = camera.WorldToScreenPoint(objectCorners[i]); // Transform world space position of corner to screen space
+            if (screenBounds.Contains(tempScreenSpaceCorner)) // If the corner is inside the screen
+            {
+                visibleCorners++;
+            }
+        }
+        return visibleCorners;
+    }
+ 
+    /// <summary>
+    /// Determines if this RectTransform is fully visible from the specified camera.
+    /// Works by checking if each bounding box corner of this RectTransform is inside the cameras screen space view frustrum.
+    /// </summary>
+    /// <returns><c>true</c> if is fully visible from the specified camera; otherwise, <c>false</c>.</returns>
+    /// <param name="rectTransform">Rect transform.</param>
+    /// <param name="camera">Camera.</param>
+    public static bool IsFullyVisibleFrom(this RectTransform rectTransform, Camera camera)
+    {
+        return CountCornersVisibleFrom(rectTransform, camera) == 4; // True if all 4 corners are visible
+    }
+ 
+    /// <summary>
+    /// Determines if this RectTransform is at least partially visible from the specified camera.
+    /// Works by checking if any bounding box corner of this RectTransform is inside the cameras screen space view frustrum.
+    /// </summary>
+    /// <returns><c>true</c> if is at least partially visible from the specified camera; otherwise, <c>false</c>.</returns>
+    /// <param name="rectTransform">Rect transform.</param>
+    /// <param name="camera">Camera.</param>
+    public static bool IsVisibleFrom(this RectTransform rectTransform, Camera camera)
+    {
+        return CountCornersVisibleFrom(rectTransform, camera) > 0; // True if any corners are visible
     }
 }
